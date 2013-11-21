70235e9d0cd6bb675dabd2d3614084f55f0f5c3e
Rizwan Ahmad <rizwanahmad93@gmail.com>
2013-11-20 17:06:38 -0700

    Added timeouts and 20 requests per client run. Also have a todo list at the top of udpClient

--- udpClient.c
+++ udpClient.c
@@ -1,10 +1,17 @@
+/* TODO: 
+ * - Client IP address
+ * - Incarnation number
+ * - Request logic on client and server side
+ * - Simulating failures
+ */
+
 #include <stdio.h>		/* for printf() and fprintf() */
 #include <sys/socket.h> /* for socket(), connect(), sendto(), and recvfrom() */
 #include <arpa/inet.h>	/* for sockaddr_in and inet_addr() */
 #include <stdlib.h>		/* for atoi() and exit() */
 #include <string.h>		/* for memset() */
 #include <unistd.h>		/* for close() */
-#include <time.h>
+#include <time.h>		/* for srand(time()) */
 
 #define ECHOMAX 255		/* Longest string to echo */
 
@@ -30,10 +37,12 @@ int main(int argc, char *argv[])
 	unsigned short echoServPort;		/* Echo server port */
 	unsigned int fromSize;				/* In-out of address size for recvfrom() */
 	char *servIP;						/* IP address of server */
-	struct request clientReqPointer;	/* Pointer to clientRequest */
+	struct request clientRequest;		/* Pointer to clientRequest */
+	struct timeval timer;				/* Timeval struct for timeouts */
 	char echoBuffer[5];					/* Buffer for receiving echoed string */
 	int echoStringLen;					/* Length of string to echo */
 	int respStringLen;					/* Length of received response */
+	int i;								/* Loop counter */
 
 	if ((argc < 3) || (argc > 4))		/* Test for correct number of arguments */
 	{
@@ -42,12 +51,8 @@ int main(int argc, char *argv[])
 	}
 	
 	srand(time(0));						/* Seed the random character generator */
-	
-	strcpy(clientReqPointer.client_ip, "333.333.333.333");
-	clientReqPointer.inc = 10;
-	clientReqPointer.client = atoi(argv[3]);
-	clientReqPointer.req = 0;
-	clientReqPointer.c = rand()%26+97;	
+	timer.tv_sec = 1;					/* Set timeout to one second */
+	timer.tv_usec = 0;
 
 	servIP = argv[1];			/* First arg: server IP address (dotted quad) */
 
@@ -59,6 +64,10 @@ int main(int argc, char *argv[])
 	/* Create a datagram/UDP socket */
 	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
 		DieWithError("socket() failed");
+	
+	/* Set the timeout interval of the socket */
+	if(setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timer, sizeof(struct timeval)) != 0)
+		DieWithError("setsockopt() failed");
 
 	/* Construct the server address structure */
 	memset(&echoServAddr, 0, sizeof(echoServAddr));		/* Zero out structure */
@@ -66,26 +75,35 @@ int main(int argc, char *argv[])
 	echoServAddr.sin_addr.s_addr = inet_addr(servIP);	/* Server IP address */
 	echoServAddr.sin_port	= htons(echoServPort);		/* Server port */
 
-	/* Send the string to the server */
-	if (sendto(sock, &clientReqPointer, sizeof(clientReqPointer), 0, (struct sockaddr *)
-		&echoServAddr, sizeof(echoServAddr)) != sizeof(clientReqPointer))
+	for(i = 0; i <= 20; i++) {
+		
+		strcpy(clientRequest.client_ip, "333.333.333.333");
+		clientRequest.inc = 10;
+		clientRequest.client = atoi(argv[3]);
+		clientRequest.req = i;
+		clientRequest.c = rand()%26+97;
+		
+		/* Send the request to the server */
+		if (sendto(sock, &clientRequest, sizeof(clientRequest), 0, (struct sockaddr *)
+		&echoServAddr, sizeof(echoServAddr)) != sizeof(clientRequest))
 			DieWithError("sendto() sent a different number of bytes than expected");
   
-	/* Recv a response */
-	fromSize = sizeof(fromAddr);
-	if ((respStringLen = recvfrom(sock, echoBuffer, 5, 0, 
-	(struct sockaddr *) &fromAddr, &fromSize)) != 5)
-		DieWithError("recvfrom() failed");
+		/* Recv a response */
+		fromSize = sizeof(fromAddr);
+		if ((respStringLen = recvfrom(sock, echoBuffer, 5, 0, 
+		(struct sockaddr *) &fromAddr, &fromSize)) != 5)
+			DieWithError("recvfrom() failed");
 
-	if (echoServAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr)
-	{
-		fprintf(stderr,"Error: received a packet from unknown source.\n");
-		exit(1);
+		if (echoServAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr)
+		{
+			fprintf(stderr,"Error: received a packet from unknown source.\n");
+			exit(1);
+		}
+		
+		/* null-terminate the received data */
+		echoBuffer[5] = '\0';
+		printf("Received: %s\n", echoBuffer);	/* Print the echoed arg */
 	}
-
-	/* null-terminate the received data */
-	echoBuffer[5] = '\0';
-	printf("Received: %s\n", echoBuffer);	/* Print the echoed arg */
 	
 	close(sock);
 	exit(0);
